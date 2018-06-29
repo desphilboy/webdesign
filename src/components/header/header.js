d978ba5e63f3fa27a9f5e6deb1aa2107f0108ade
Iman Dezfuly <desphilboy@yahoo.com>
2018-06-29 16:00:06 +1000

    save my work

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,11 @@
 import React, { Component } from  'react';
+import './header.less';
 
 
 export class Header extends Component {
 
 	render() {
-		return <div>
+		return <div className='webdesign-header'>
 		<h1> Component Header fffyay!! </h1>
 		</div>;
 	}
